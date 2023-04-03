301de1d97126fd6c90eead59a00eaae5ba1163f3
Danilo Lucas <developer.dlio@gmail.com>
2023-04-03 14:18:49 -0300

    update package

--- dist/index.js
+++ dist/index.js
@@ -20729,51 +20729,39 @@ var __webpack_exports__ = {};
 const { readFileSync, writeFileSync, readdirSync } = __nccwpck_require__(7147);
 const { join } = __nccwpck_require__(1017);
 const core = __nccwpck_require__(2186);
-const $ = __nccwpck_require__(3571);
+const translator = __nccwpck_require__(3571);
 const unified = __nccwpck_require__(5075);
 const parse = __nccwpck_require__(4859);
 const stringify = __nccwpck_require__(7114);
 const visit = __nccwpck_require__(199);
 const simpleGit = __nccwpck_require__(9103);
+
 const git = simpleGit();
 
-const toAst = (markdown) => {
-  return unified().use(parse).parse(markdown);
-};
-
-const toMarkdown = (ast) => {
-  return unified().use(stringify).stringify(ast);
-};
-
 const mainDir = ".";
-let README = readdirSync(mainDir).includes("readme.md")
-  ? "readme.md"
-  : "README.md";
+
+let README = readdirSync(mainDir).includes("readme.md") ? "readme.md" : "README.md";
+
 const lang = core.getInput("LANG") || "zh-CN";
 const readme = readFileSync(join(mainDir, README), { encoding: "utf8" });
-const readmeAST = toAst(readme);
-console.log("AST CREATED AND READ");
+const readmeAST = unified().use(parse).parse(readme);
 
-// let originalText = [];
+let originalText = [];
 
 visit(readmeAST, async (node) => {
   if (node.type === "text") {
-    // originalText.push(node.value);
-    node.value = (await $(node.value, { to: lang })).text;
+    originalText.push(node.value);
+    node.value = (await translator(node.value, { to: lang })).text;
   }
 });
 
-// const translatedText = originalText.map(async (text) => {
-//   return (await $(text, { to: lang })).text;
-// });
+const translatedText = originalText.map(async (text) => {
+  return (await translator(text, { to: lang })).text;
+});
 
 async function writeToFile() {
-  // await Promise.all(translatedText);
-  writeFileSync(
-    join(mainDir, `README.${lang}.md`),
-    toMarkdown(readmeAST),
-    "utf8"
-  );
+  await Promise.all(translatedText);
+  writeFileSync(join(mainDir, `README.${lang}.md`), unified().use(stringify).stringify(readmeAST), "utf8");
   console.log(`README.${lang}.md written`);
 }
 
@@ -20783,7 +20771,7 @@ async function commitChanges(lang) {
   await git.add(`./README.${lang}.md`);
   await git.addConfig("user.name", "github-actions[bot]");
   await git.addConfig("user.email", "41898282+github-actions[bot]@users.noreply.github.com");
-  await git.commit(`docs: Added README.${lang}.md translation via https://github.com/dliocode/translate-md`);
+  await git.commit(`Added README.${lang}.md - https://github.com/dliocode/translate-md`);
   console.log("finished commit");
   await git.push();
   console.log("pushed");
